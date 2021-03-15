52aee959108ec26c6195c4d1a8aad3524616a291
David Varnai <shaxvideos@gmail.com>
2021-03-15 15:40:52 +0100

    V 0.762

--- django_blog/asgi.py
+++ django_blog/asgi.py
@@ -4,4 +4,4 @@ from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
 django.setup()
-application = get_default_application
+application = get_default_application()
